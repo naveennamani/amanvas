aeaeaea1c7bbde38ef52975d7b9fbabe7244c1bf
Naveen Namani <naveennamani877@gmail.com>
2020-11-26 16:41:18 +0530

    moving from table in html to javascript
    * the table generation is moved to javascript which generates the table based on the table_structure variable used to finetune the type of fields
    * added a heading "Data entry"
    * styled the date with the label
    * added a button to save the data
    * added a div.card-columns to show the cards for each date the data is stored
    * all used variables are converted to globals and initialized in window.onload
    * making date input element to required
    * added save_data and update_saved_data functions

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-let t = document.getElementById("all_values");
+let all_values_table, all_values_form, saved_data_div;
 let table_structure = [
     ["Field", "Normal", "&nbsp", "TOD KWH", "TOD KVAH", "TOD MD"],
     ["KWH", {"type": "text"}, "T1", {"type": "text"}, {"type": "text"}, {"type": "text"}],
@@ -13,8 +13,8 @@ let table_structure = [
     ["Seals", {"type": "text"}, {"type": "text"}, {"type": "text"}, {"type": "text"}, {"type": "text"}],
 ];
 function gen_table() {
-    let s = `<table id = "all_values" class = "table table-striped table-bordered table-hover table-responsive">`;
-    s += '<thead class = "thead-dark"><tr>'
+    // let s = `<table id = "all_values" class = "table table-striped table-bordered table-hover table-responsive">`;
+    let s = '<thead class = "thead-dark"><tr>'
     for(let table_head_col of table_structure[0]) {
         s += `<th scope = "col">${table_head_col}</th>`;
     }
@@ -24,12 +24,12 @@ function gen_table() {
         if(index == 0) return;
         s += '<tr>';
         for(let col of row) {
-            console.log(typeof(col))
+            // console.log(typeof(col))
             if(typeof(col)=="string") {
                 s += `<td>${col}</td>`;
             } else {
                 if(col["type"]=="datetime") {
-                    s += '<td style = "width: 200px;"><input type="date" class = "form-control"/><br/><input type="time" class = "form-control"/></td>'
+                    s += '<td style = "width: 200px;"><input type="date" class = "form-control" required/><br/><input type="time" class = "form-control"/></td>'
                 } else if(col["type"]=="text") {
                     s += '<td style = "width: 200px;"><input type="text" class = "form-control"/></td>'
                 } else {
@@ -39,10 +39,54 @@ function gen_table() {
         }
         s += '</tr>';
     });
+    all_values_table.innerHTML = s;
     return s;
 }
+function save_data(f) {
+    // console.log(f);
+    let values = [];
+    let all_input_fields = all_values_form.querySelectorAll("input");
+    let input_element_index = 1;
+    table_structure.forEach(function(row, index) {
+        values[index] = [];
+        row.forEach(function(field, index2) {
+            if(typeof(field)=="object") {
+                values[index][index2] = all_input_fields[input_element_index++].value;
+            }
+        });
+    });
+    console.log(values);
+    let all_fields = JSON.parse(localStorage.getItem("all_fields"));
+    if(all_fields == null) all_fields = {};
+    all_fields[all_input_fields[0].value] = values;
+    localStorage.setItem("all_fields", JSON.stringify(all_fields));
+    update_saved_data();
+    return false;
+}
+function update_saved_data() {
+    let all_fields = JSON.parse(localStorage.getItem("all_fields"));
+    let htm = "";
+    for(let field_date in all_fields) {
+        console.log(field_date);
+        htm += `
+            <div class = "card">
+                <h1 class = "card-header">${field_date}</h1>
+                <div class = "card-body">
+                    <div class = "btn-group-vertical" role = "group">
+                        <button type = "button" class = "btn btn-primary" onclick = "show_data('${field_date}');">Show data</button>
+                        <button tpye = "button" class = "btn btn-primary" onclick = "edit_data('${field_date}');">Edit data</button>
+                    </div>
+                </div>
+            </div>
+        `;
+    }
+    saved_data_div.innerHTML = htm;
+}
 window.onload = function() {
-    let d = document.createElement("div");
-    d.innerHTML = gen_table();
-    document.body.appendChild(d);
+    all_values_table = document.getElementById("all_values");
+    all_values_form = document.getElementById("all-values-form");
+    saved_data_div = document.getElementById("saved_data");
+    gen_table();
+    all_values_form.onsubmit = save_data;
+    update_saved_data();
 };
